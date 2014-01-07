0a25ac05878bfc9bee8bc795a07575b4f785f591
Daniel Hodan <daniel.hodan@czertbytes.de>
2014-01-07 17:15:52 +0100

    Initial commit

---
+++ hello.go
@@ -0,0 +1,36 @@
+package hello
+
+import (
+    "log"
+    "net/http"
+    "net/url"
+
+    "github.com/rcrowley/go-tigertonic"
+)
+
+var (
+    mux, nsMux *tigertonic.TrieServeMux
+)
+
+type HelloResponse struct {
+    Message string `json:"message"`
+}
+
+func init() {
+    log.SetFlags(log.Ltime | log.Lmicroseconds | log.Lshortfile)
+
+    cors := tigertonic.NewCORSBuilder().AddAllowedOrigins("*")
+
+    mux = tigertonic.NewTrieServeMux()
+    mux.Handle("GET", "/hello", cors.Build(tigertonic.Timed(tigertonic.Marshaled(hello), "hello", nil)))
+
+    nsMux = tigertonic.NewTrieServeMux()
+    nsMux.HandleNamespace("", mux)
+    nsMux.HandleNamespace("/1.0", mux)
+
+    http.Handle("/", nsMux)
+}
+
+func hello(u *url.URL, h http.Header, _ interface{}) (int, http.Header, *HelloResponse, error) {
+    return http.StatusOK, nil, &HelloResponse{"Saki Saki Saki"}, nil
+}
---
+++ hello_test.go
@@ -0,0 +1,38 @@
+package hello
+
+import (
+    "net/http"
+    "testing"
+
+    "github.com/rcrowley/go-tigertonic"
+    "github.com/rcrowley/go-tigertonic/mocking"
+)
+
+var (
+    hMux tigertonic.HostServeMux
+)
+
+func init() {
+    hMux = tigertonic.NewHostServeMux()
+    hMux.Handle("example.com", nsMux)
+}
+
+func TestHello(t *testing.T) {
+    code, _, response, err := hello(
+        mocking.URL(hMux, "GET", "http://example.com/1.0/hello"),
+        mocking.Header(nil),
+        nil,
+    )
+
+    if nil != err {
+        t.Fatal(err)
+    }
+
+    if http.StatusOK != code {
+        t.Fatal(code)
+    }
+
+    if "Saki Saki Saki" != response.Message {
+        t.Fatal(response)
+    }
+}
